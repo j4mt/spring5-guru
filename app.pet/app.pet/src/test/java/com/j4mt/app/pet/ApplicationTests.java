54bfe52a65250562af1ab48dd78bb30d05d88aa7
john.orourke <john.orourke@workhuman.com>
2021-09-15 10:54:24 +0100

    Assignment 1

---
+++ app.pet/app.pet/src/test/java/com/j4mt/app/pet/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j4mt.app.pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
